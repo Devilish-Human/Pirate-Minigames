5a2a55a06937ed5f60db87c62c9b4d4e73c2681c
Devilish-Human <prakashpokhrel19@gmail.com>
2023-06-08 02:43:53 -0400

    ads

--- src/client/KnitRuntimeClient.client.lua
+++ src/client/KnitRuntimeClient.client.lua
@@ -19,18 +19,17 @@ local KNITTAG = "[KNIT]"
 for _, v in ipairs(script.Parent:GetDescendants()) do
     local IsModule = v:IsA("ModuleScript")
     local IsService = v:IsDescendantOf(script.Parent.Controllers) and v.Name:match("Controller$")
-    local IsComponent = v:IsDescendantOf(script.Parent.Components) and v.Name:match("Component$")
+    --local IsComponent = v:IsDescendantOf(script.Parent.Components) and v.Name:match("Component$")
     
     if (IsModule) then
         if (IsService) then
             require(v)
             print(`{CLIENTTAG} Loaded controller {v.Name}.`)
-        else if (IsComponent) then
-            Knit.Components[v.Name] = require(v)
-            print(`{CLIENTTAG} Loaded component {v.Name}.`)
+        -- else if (IsComponent) then
+        --     Knit.Components[v.Name] = require(v)
+        --     print(`{CLIENTTAG} Loaded component {v.Name}.`)
         else
             Knit.Modules[v.Name] = v
-        end
         end
     end
 end
